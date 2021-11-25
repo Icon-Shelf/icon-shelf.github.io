ef0a2076499f7e51793e3900eefed7cab62174ac
MrRobz <robinphilipthomas@outlook.in>
2021-11-25 22:48:18 +0530

    feat: wire-up download functionality

--- src/components/download-page/DownloadList/DownloadItem.jsx
+++ src/components/download-page/DownloadList/DownloadItem.jsx
@@ -1,15 +1,11 @@
 import * as React from "react";
-import { Button } from "../../atoms/button";
 
-const DownloadItem = ({ children, actionText }) => {
+const DownloadItem = ({ children, action }) => {
   return (
     <div className="flex flex-col items-center justify-end pb-5 w-72 h-72 md:w-80 border-2 border-mockup rounded-lg">
       <div>{children}</div>
-      <div className="mt-8 w-full px-4">
-        <Button size="large" className="w-full">
-          {actionText}
-        </Button>
-      </div>
+
+      <div className="mt-8 w-full px-4">{action}</div>
     </div>
   );
 };
--- src/components/download-page/index.jsx
+++ src/components/download-page/index.jsx
@@ -3,7 +3,7 @@ import DownloadList from "./DownloadList";
 
 const DownloadPage = () => {
   return (
-    <section className="py-12 md:py-20 m-auto max-w-screen-xl px-6 md:mx-auto">
+    <section className="py-12 md:py-20 m-auto max-w-screen-xl px-6 md:mx-auto mb-24">
       <h1 className="text-white m-auto font-medium mx-6 text-mobile-h1 text-center md:text-desktop-h1 md:text-5xl">
         Download Icon Shelf
       </h1>
