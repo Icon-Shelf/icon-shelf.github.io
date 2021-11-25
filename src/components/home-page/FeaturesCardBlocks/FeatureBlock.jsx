d143aee16771508f71592e3e0735a210ab34d558
MrRobz <robinphilipthomas@outlook.in>
2021-11-25 17:16:46 +0530

    feaet: add coming soon pill

--- src/components/home-page/FeaturesCardBlocks/FeatureBlock.jsx
+++ src/components/home-page/FeaturesCardBlocks/FeatureBlock.jsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
-export const FeatureBlock = ({ img, heading, description }) => (
-  <div className="flex ml-6 mr-6 md:ml-0 md:w-1/3 mt-8 md:mr-8">
+export const FeatureBlock = ({ img, heading, description, comingSoon }) => (
+  <div className="relative flex ml-6 mr-6 md:ml-0 md:w-1/3 mt-8 md:mr-8">
+    {comingSoon && (
+      <div class="absolute rounded-full py-3 px-6 bg-primary text-xs transform origin-bottom -rotate-45 mt-7">
+        COMING SOON
+      </div>
+    )}
+
     <div className="w-full text-center p-9 rounded-3xl bg-mockup-b">
       <div className="w-12 h-12 p-3 m-auto rounded-full bg-slate-orange">
         {img}
--- src/components/home-page/FeaturesCardBlocks/index.jsx
+++ src/components/home-page/FeaturesCardBlocks/index.jsx
@@ -25,6 +25,7 @@ const FeaturesCardBlocks = () => {
               description={
                 "Easily drag icons from Iconset to your design tool."
               }
+              comingSoon={true}
             />
             <FeatureBlock
               img={<BeakerIcon className="w-6" />}
